6b0d980378744a67363a80c504783fa62f8545b9
Idan Arye <IdanArye@gmail.com>
2024-03-04 20:31:43 +0200

    Start working on the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+//! Yoetz - A Rule Based AI Plugin for the Bevy Game Engine
+//!
+//! Yoetz ("advisor" in Hebrew) is a rule based AI plugin for Bevy, structured around the following
+//! tenants:
+//!
+//! 1. There is no need to build special data structures for calculating the transitions and the
+//!    scores when representing these mechanisms as code inside user systems is both more flexible
+//!    and simpler to use.
+//! 2. The systems that check the rules need to be able to pass data to the systems act upon the
+//!    decisions.
+//! 3. Enacting the decision should be done with the ECS. If the action that the rules mechanism
+//!    decided to do is reflected by components, it becomes easy to write different systems that
+//!    perform the various possible actions.
+//!
+//! # Quick Start
+//!
+//! Define the various actions the AI can do with an enum that derives
+//! [`YoetzAdvisor`](crate::advisor::YoetzAdvisor):
+//!
+//! ```no_run
+//! # use bevy::prelude::*;
+//! # use bevy_yoetz::prelude::*;
+//! #[derive(YoetzSuggestion)]
+//! enum AiBehavior {
+//!     DoNothing,
+//!     Attack {
+//!         #[yoetz(key)]
+//!         target_to_attack: Entity,
+//!     },
+//! }
+//! ```
 mod advisor;
 
 use std::marker::PhantomData;
