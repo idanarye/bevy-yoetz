59d55e24c7289d0576a0c4b305b8a5183b5a7a4f
Seivan <seivan.heidari@icloud.com>
2024-07-14 16:50:12 +0200

    Run `act` systems in `Update`
    The would need to be lerped for it to be proper, but unnecessary for now.

--- examples/example.rs
+++ examples/example.rs
@@ -25,15 +25,17 @@ fn main() {
                     enemies_detect_player,
                     enemies_in_distance_for_circling,
                 )
-                    .in_set(YoetzSystemSet::Suggest),
+                    .in_set(YoetzSystemSet::Suggest)
+            ),
+        )
+        .add_systems(Update,
                 (
                     // These systems match with the strategies the AI decided on, and enact them.
                     enemies_do_nothing,
                     enemies_follow_player,
                     enemies_circle_player,
                 )
-                    .in_set(YoetzSystemSet::Act),
-            ),
+                    .in_set(YoetzSystemSet::Act)
         )
         .add_systems(
             Update,
